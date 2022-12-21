0adb71349db25f59f7e8cc1354196c36bee4702e
Shirshajit Sen Gupta <shirshajit@gmail.com>
2022-12-22 04:48:50 +0800

    exec permission update

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -3,6 +3,6 @@ if [ -f "$FILE" ]; then
     rm $FILE
 fi
 docker run --rm -v "$PWD:/srv/jekyll/" -p "8080:8080" \
-                    -it al-folio:latest bundler  \
+                    -it shirsho-12.github.io:latest bundler  \
                     exec jekyll serve --watch --port=8080 --host=0.0.0.0 \
                     --verbose --livereload
