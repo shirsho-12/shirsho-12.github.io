659a690d0e3a1bd73691ff90f9c6513d4c2a7b3a
Shirshajit Sen Gupta <shirshajit@gmail.com>
2025-04-25 17:48:26 +0800

    Fix: Update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -6,20 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/', // For personal GitHub Pages site
+  base: "/shirsho-12/", // For personal GitHub Pages site
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ["**/*.md"], // Add markdown files as assets
+  assetsInclude: ["**/*.md", "**/*.jpg", "**/*.png"],
 }));
