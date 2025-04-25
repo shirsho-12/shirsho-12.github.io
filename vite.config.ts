cb5d7c4fa8424643098e360c9f273c171a6cdb96
Shirshajit Sen Gupta <shirshajit@gmail.com>
2025-04-25 21:34:03 +0800

    Update vite config pate

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // For personal GitHub Pages site
+  base: "./", // For personal GitHub Pages site
   server: {
     host: "::",
     port: 8080,
